1e9bf9ad67563acbc6d95a6f441a6d709a1d6ee9
brennnnan <brennan.mackay@sbcglobal.net>
2017-11-27 20:35:47 -0800

    initial commit

---
+++ client.js
@@ -0,0 +1,148 @@
+$(function () {
+	"use strict";
+
+    var input = $('#input');
+	var midiInput;
+	var midiOutput;
+	var channelCount = -1;
+	var webMidiEnabled = 0;
+	var indicatorLight = -1;
+	var displayEnabled = -1;
+	var history = [];
+
+	
+
+	var socket = io();
+	if(socket) {
+		// Display options
+	}
+
+
+	
+
+	
+	socket.on('noteOn', function(noteInfo) {
+		if(displayEnabled && myName!='admin') {
+			if(noteInfo.note >= 0 && noteInfo.note < 128) {
+				history.push(noteInfo.note);
+				indicatorLight.style.background = "#E066FF";
+				notes[(noteInfo.note+2)%12].play();
+				//else midiOutput.playNote(noteInfo.note);
+			}
+		}
+	})
+	
+	socket.on('noteOff', function(noteInfo) {
+		if(displayEnabled && myName!='admin') {
+			if(noteInfo.note >= 0 && noteInfo.note < 128) {
+				indicatorLight.style.background = "#FFF";
+				if(!webMidiEnabled) midiOutput.stopNote(noteInfo.note)
+			}
+		}
+	})
+	
+	socket.on('receipt', function(receiptInfo) {
+		console.log('user is in group '+receiptInfo.group)
+		
+		// loads appropriate ui depending on user type
+		if(receiptInfo.group == -1) loadAdminInfoInterface();
+		else loadServantInfoInterface();
+	})
+	
+	socket.on('adminNotification', function() {
+		masterButton.removeEventListener('click', rEvent, true);
+	})
+	
+	// Confirms landing page selection with server
+	// sends role as 'name' msg
+	function roleSelected(role) {
+		if(role===0) {
+			myName = 'admin'
+			var obj = {
+				role: 'admin'
+			}
+			socket.emit('name', obj);
+		} else if(role==1) {
+			var obj = {
+				role: 'servant'
+			}
+			socket.emit('name', obj);
+		}
+	}
+    
+    function loadAudio() {
+        
+    }
+    
+    function transferAudio(audioFile) {
+        delivery.send({
+          name: 'sample-image.jpg',
+          path : './sample-image.jpg'
+        });
+    }
+	
+	// Attempts to activate webMidi and sets up, otherwise loads audio files
+	function enableMidi(role) {
+		WebMidi.enable(function (err) {
+  
+    		if (err) {
+      			console.log("WebMidi could not be enabled.", err);
+				loadSounds();
+    		} else {
+				webMidiEnabled = 1;
+      			console.log("WebMidi enabled!");
+      			console.log(WebMidi.inputs);
+      			console.log(WebMidi.outputs);
+				
+				
+				if(role=='admin') {
+					// Sets up select dropdown for midi inputs and channels
+					var midiInputs = document.getElementById('midi_input_list')
+					var options = [];
+		  			for(var d=0; d<WebMidi.inputs.length; d++) {
+		  				options.push(WebMidi.inputs[d].name);
+					}
+					if(options.length>0) midiInput = WebMidi.getInputByName(options[0])
+					for (var i = 0; i < options.length; i++) {
+		    			var option = document.createElement('option');
+		    			option.value = options[i];
+		    			option.innerHTML = options[i];
+		    			midiInputs.appendChild(option);
+		  			}
+
+					midiInputs.onchange = function() {
+						midiInput = WebMidi.getInputByName(this.value);
+  					}
+					var channels = document.getElementById('channel_list')
+					channels.onchange = function() {
+						channelCount = this.value;
+						console.log(channelCount)
+					}
+		  		}
+				
+				if(role=='servant') {
+					// sets up select dropdown for midi outputs 
+					var midiOutputs = document.getElementById('midi_output_list')
+					var options = [];
+		  			for(var d=0; d<WebMidi.outputs.length; d++) {
+		  				options.push(WebMidi.outputs[d].name);
+					}
+					for (var i = 0; i < options.length; i++) {
+		    			var option = document.createElement('option');
+		    			option.value = options[i];
+		    			option.innerHTML = options[i];
+		    			midiOutputs.appendChild(option);
+		  			}
+					if(options.length>0) midiOutput = WebMidi.getOutputByName(options[0])
+					midiOutputs.onchange = function() {
+						midiOutput = WebMidi.getOutputByName(this.value);
+  					}
+				}
+				
+			}
+    	});
+	}
+
+
+
+});
---
+++ server.js
@@ -0,0 +1,60 @@
+'use strict';
+// list of currently connected clients (users)
+var clients = [];
+//Setup
+var http = require('http');
+var path = require('path');
+var express = require('express')
+    , app = module.exports.app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'public')));
+var server = http.createServer(app);
+
+var connectionCount = 0;
+var serverChannelCount = 1;
+
+
+var io = require('socket.io').listen(server); //pass a http.Server instance
+server.listen(PORT); //listen on port 
+console.log('Server listening on port ' + PORT);
+
+
+io.on('connection', (socket) => {
+
+    //var index = clients.push([socket, clients.length, 0]) - 1;
+    connectionCount++;
+    // Notify new connections if admin is present so that only servant option is available
+
+    var userRole = false;
+    console.log((new Date()) + ' Connection accepted.');
+    
+    
+    socket.on('login', (userInfo) => {
+            userRole = name.role;
+            console.log((new Date()) + ' User is present with ' + name.channels + ' channels.');
+            clients[index][2] == 1;
+            var obj = {
+                group: -1
+            }
+            socket.emit('receipt', obj);
+    })
+    
+    delivery = dl.listen( socket );
+    delivery.connect();
+
+    delivery.on('delivery.connect',function(delivery){
+        delivery.send({
+            name: 'sample-image.jpg',
+            path : './sample-image.jpg'
+        });
+
+        delivery.on('send.success',function(file){
+          console.log('File sent successfully!');
+        });
+    });
+    
+    
+    socket.on('disconnect', () => {
+        connectionCount--;
+    });
+});
